2eae6c054b6d49336aca17977d5bc45764de0dd9
Jeremy Palmer <jpalmer@linz.govt.nz>
2011-03-18 00:59:47 +0000

    Renamed old bde_geo references to linz_bde_uploader.
    git-svn-id: svn://localhost/linz_bde_uploader@1091 40957e5d-2553-1d4f-9b5d-fe299c199263

---
+++ script/run_upload.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Script to run the linz_bde_uploader perl script, rotating log files
+
+SCRIPTDIR=$(dirname $(readlink -f $0))
+cd $SCRIPTDIR
+
+LOGDIR=../log
+SCRIPTLOG=bde_geo.log
+RUNLOG=bde_geo_run.log
+LAST=11
+
+for i in 10 9 8 7 6 5 4 3 2 1
+do
+   rm -f $LOGDIR/$SCRIPTLOG.$LAST >/dev/null 2>&1
+   rm -f $LOGDIR/$RUNLOG.$LAST >/dev/null 2>&1
+   mv $LOGDIR/$SCRIPTLOG.$i $SCRIPTLOG.$LAST >/dev/null 2>&1
+   mv $LOGDIR/$RUNLOG.$i $RUNLOG.$LAST >/dev/null 2>&1
+   LAST=$i
+done
+
+
+mv $LOGDIR/$SCRIPTLOG $SCRIPTLOG.1 >/dev/null 2>&1
+mv $LOGDIR/$RUNLOG $RUNLOG.1 >/dev/null 2>&1
+
+perl linz_bde_uploader.pl -verbose -listing $LOGDIR/$SCRIPTLOG $@ >$LOGDIR/$RUNLOG 2>&1
