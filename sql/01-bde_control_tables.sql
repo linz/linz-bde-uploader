linz/linz-bde-uploader

commit 04afc706f9bfce57baea3823106ac57ca20e48f8

feat: only alter table owner if really needed
Reduces the odds of deadlocks.
Fixes #256

sql/01-bde_control_tables.sql
@@ -49,7 +49,13 @@ CREATE TABLE IF NOT EXISTS bde_control.upload
     status CHAR(1) NOT NULL DEFAULT 'U'
 );
 
-ALTER TABLE bde_control.upload OWNER TO bde_dba;
+IF u.usename != 'bde_dba'
+    FROM pg_user u, pg_class c
+    WHERE u.usesysid = c.relowner
+    AND c.oid = 'bde_control.upload'::regclass
+THEN
+    ALTER TABLE bde_control.upload OWNER TO bde_dba;
+END IF;
 
 COMMENT ON TABLE bde_control.upload IS
 $comment$
@@ -94,7 +100,13 @@ CREATE TABLE IF NOT EXISTS bde_control.upload_table
     UNIQUE (schema_name,table_name)
 );
 
-ALTER TABLE bde_control.upload_table OWNER TO bde_dba;
+IF u.usename != 'bde_dba'
+    FROM pg_user u, pg_class c
+    WHERE u.usesysid = c.relowner
+    AND c.oid = 'bde_control.upload_table'::regclass
+THEN
+    ALTER TABLE bde_control.upload_table OWNER TO bde_dba;
+END IF;
 
 COMMENT ON TABLE bde_control.upload_table IS
 'Tracks the status of uploads for each table.';
@@ -181,7 +193,13 @@ CREATE TABLE IF NOT EXISTS bde_control.upload_stats
 PERFORM pg_temp.createIndexIfNotExists('idx_sts_tbl', 'bde_control', 'upload_stats', 'tbl_id');
 PERFORM pg_temp.createIndexIfNotExists('idx_sts_upl', 'bde_control', 'upload_stats', 'upl_id');
 
-ALTER TABLE bde_control.upload_stats OWNER TO bde_dba;
+IF u.usename != 'bde_dba'
+    FROM pg_user u, pg_class c
+    WHERE u.usesysid = c.relowner
+    AND c.oid = 'bde_control.upload_stats'::regclass
+THEN
+    ALTER TABLE bde_control.upload_stats OWNER TO bde_dba;
+END IF;
 
 COMMENT ON TABLE bde_control.upload_stats IS
 $comment$

commit 8cfb58a393c328eaafa3a1c339fd97b6fe9a09cd

fix: restore PG93 compatibility
Broken with 04afc706f9bfce57baea3823106ac57ca20e48f8
See #256

sql/01-bde_control_tables.sql
@@ -49,9 +49,9 @@ CREATE TABLE IF NOT EXISTS bde_control.upload
     status CHAR(1) NOT NULL DEFAULT 'U'
 );
 
-IF u.usename != 'bde_dba'
-    FROM pg_user u, pg_class c
-    WHERE u.usesysid = c.relowner
+IF r.rolname != 'bde_dba'
+    FROM pg_roles r, pg_class c
+    WHERE r.oid = c.relowner
     AND c.oid = 'bde_control.upload'::regclass
 THEN
     ALTER TABLE bde_control.upload OWNER TO bde_dba;
@@ -100,9 +100,9 @@ CREATE TABLE IF NOT EXISTS bde_control.upload_table
     UNIQUE (schema_name,table_name)
 );
 
-IF u.usename != 'bde_dba'
-    FROM pg_user u, pg_class c
-    WHERE u.usesysid = c.relowner
+IF r.rolname != 'bde_dba'
+    FROM pg_roles r, pg_class c
+    WHERE r.oid = c.relowner
     AND c.oid = 'bde_control.upload_table'::regclass
 THEN
     ALTER TABLE bde_control.upload_table OWNER TO bde_dba;