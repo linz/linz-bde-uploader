319da0aa118a1a506f1b630b6763f814c6c18b56
Mitchell Paff <77649372+MitchellPaff@users.noreply.github.com>
2022-02-09 16:07:30 +1300

    refactor: bash linting prep 2 (#310)
    * refactor: Remove useless variable
    
    * refactor: Quote variables
    
    * refactor: Remove unnecessary exit codes
    
    * refactor: lowercase nonexported variables
    
    * refactor: address PR comments

--- t/test-upgrades.bash
+++ t/test-upgrades.bash
@@ -3,58 +3,58 @@
 set -o errexit -o noclobber -o nounset -o pipefail
 shopt -s failglob inherit_errexit
 
-UPGRADEABLE_VERSIONS="
+upgradeable_versions="
     2.5.2
     2.6.0
 "
 
-TEST_DATABASE=linz-bde-uploader-test-db
+test_database=linz-bde-uploader-test-db
 
 git fetch --tags # to get all commits/tags
 
-TMPDIR=/tmp/linz-bde-uploader-test-$$
-mkdir -p ${TMPDIR}
+tmpdir=/tmp/linz-bde-uploader-test-$$
+mkdir -p "${tmpdir}"
 
-export PGDATABASE=${TEST_DATABASE}
+export PGDATABASE="${test_database}"
 
-for ver in ${UPGRADEABLE_VERSIONS}; do
-    OWD=$PWD
+for ver in ${upgradeable_versions}; do
+    owd="$PWD"
 
-    dropdb --if-exists ${TEST_DATABASE}
-    createdb ${TEST_DATABASE} || exit 1
+    dropdb --if-exists "${test_database}"
+    createdb "${test_database}"
 
     psql -XtA <<EOF
 CREATE SCHEMA IF NOT EXISTS _patches;
 CREATE EXTENSION IF NOT EXISTS dbpatch SCHEMA _patches;
 EOF
 
-    cd ${TMPDIR}
+    cd "${tmpdir}"
     test -d linz-bde-uploader || {
-        git clone --quiet --reference $OWD \
-            https://github.com/linz/linz-bde-uploader || exit 1
+        git clone --quiet --reference "$owd" \
+            https://github.com/linz/linz-bde-uploader
     }
-    cd linz-bde-uploader || exit 1
-    git checkout ${ver} || exit 1
-    ./configure && make || exit 1
-    sudo env "PATH=$PATH" make install DESTDIR=$PWD/inst || exit 1
+    cd linz-bde-uploader
+    git checkout "${ver}"
+    ./configure && make
+    sudo env "PATH=$PATH" make install DESTDIR="$PWD/inst"
 
     # Install the just-installed linz-bde-uploader first !
-    linz-bde-schema-load --revision ${TEST_DATABASE} || exit 1
+    linz-bde-schema-load --revision "${test_database}"
     for file in inst/usr/local/share/linz-bde-uploader/sql/*.sql
     do
         echo "Loading $file from linz-bde-uploader ${ver}"
-        psql -o /dev/null -XtA -f $file ${TEST_DATABASE} --set ON_ERROR_STOP=1 || exit 1
+        psql -o /dev/null -XtA -f "$file" "${test_database}" --set ON_ERROR_STOP=1
     done
 
-    cd ${OWD}
+    cd "${owd}"
 
 # Turn DB to read-only mode, as it would be done
 # by linz-bde-uploader-schema-load --readonly
-    cat <<EOF | psql -Xat ${TEST_DATABASE}
+    cat <<EOF | psql -Xat "${test_database}"
 REVOKE UPDATE, INSERT, DELETE, TRUNCATE
     ON ALL TABLES IN SCHEMA bde_control
     FROM bde_dba, bde_admin, bde_user;
 EOF
-    pg_prove -d ${TEST_DATABASE} t/ || exit 1
+    pg_prove -d "${test_database}" t/
 
 done
